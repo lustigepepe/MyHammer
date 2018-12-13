ac84e6b6e425341e4d0ad517a09c15c64f1c17e9
lustigepepe <lustigepepe@web.de>
2018-12-13 20:46:06 +0100

    preversion template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { DataPromiseService } from './services/data.promis.service';
-import { JsonClass } from './json';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +8,12 @@ import { JsonClass } from './json';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  // @Input() json: JsonClass;
   data: Array<Object> = [];
   constructor(private dataService: DataPromiseService) { }
 
   ngOnInit(): void {
 
-    this.dataService.getJsonData().then(data => { console.log(data.body) });
-    // console.log(this.jsonData);
+    this.dataService.getJsonData()
+    .then(data => { this.data = data.body, console.log(data.body) })
   }
 }
--- src/app/services/data.promis.service.ts
+++ src/app/services/data.promis.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { JsonClass } from '../json';
 
 @Injectable({ providedIn: 'root' })
 export class DataPromiseService {
    
-    private dataUrl = './assets/jobs.jso';  // URL to web api
+    private dataUrl = './assets/jobs.json';  // URL to web api
 
     constructor(private http: HttpClient) {}
 
