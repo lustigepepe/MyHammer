6aa8e4e921aea379b97944c2134911f91a3507d7
lustigepepe <lustigepepe@web.de>
2018-12-14 01:48:10 +0100

    good responive version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,22 @@ import { DataPromiseService } from './services/data.promis.service';
 })
 export class AppComponent implements OnInit {
   data: Array<Object> = [];
+  title: string = null;
+  description: string = null;
+  end_date: string = null;
+  created_at: string = null;
+
   constructor(private dataService: DataPromiseService) { }
-
   ngOnInit(): void {
-
     this.dataService.getJsonData()
-    .then(data => { this.data = data.body, console.log(data.body) })
+      .then(data => { this.data = data.body, console.log(data.body) });
+  }
+  selectJob(data) {
+    this.title = data.title;
+    this.description = "description: " + data.description
+    this.end_date = data.end_date;
+    this.created_at = data.created_at;
+    console.log(data.id);
+    // console.log(event.currentTarget);
   }
 }
